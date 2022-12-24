a447e8ba2b58c3cb8cef805326c7fa3926109d8d
Saqib <iamsaqib24@gmail.com>
2022-12-24 16:47:08 +0530

    checking-elements-inside-root

--- week-01/lec-01/app.js
+++ week-01/lec-01/app.js
@@ -4,7 +4,10 @@ const haeding2 = React.createElement("h2", {}, "JavaScript");
 
 const haeding3 = React.createElement("h3", {}, "React");
 
+console.log(haeding)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render([haeding, haeding2, haeding3]);
+// root.render([haeding, haeding2, haeding3]);
+
+root.render(haeding)

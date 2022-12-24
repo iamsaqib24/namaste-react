8d3f36cbc377f55c3dd663920cfa83b6eb975bc8
Saqib <iamsaqib24@gmail.com>
2022-12-24 16:24:58 +0530

    created-elements

--- week-01/lec-01/app.js
+++ week-01/lec-01/app.js
@@ -1,1 +1,10 @@
-const haeding =
+const haeding = React.createElement("h1", {}, "Hello World!");
+
+const haeding2 = React.createElement("h2", {}, "JavaScript");
+
+const haeding3 = React.createElement("h3", {}, "React");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render([haeding, haeding2, haeding3]);

b353411438834cb8383675bd5473dc264497bf61
Saqib <iamsaqib24@gmail.com>
2022-12-28 17:25:54 +0530

    jsx

--- fromScrimba/index.js
+++ fromScrimba/index.js
@@ -32,7 +32,36 @@
 
 // Imperative way of code,
 
-const h1 = document.createElement("h1")
-h1.textContent = "Imperative way of writing code"
-h1.className = "header"
-document.getElementById("root").appendChild(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "Imperative way of writing code"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1)
+
+
+// JSX
+
+// const element =  (
+//     <div>
+//         <h1 className="header">Hello JSX</h1>
+//         <p>this is a paragraph.</p>
+//     </div>
+//     )
+
+// console.log(element); // print object
+
+// ReactDOM.render(element, document.getElementById("root"))
+
+// challenge,
+
+const navbar = (
+    <nav>
+        <h1>React</h1>
+        <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Docs</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))

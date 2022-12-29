074bdb8a4f33f1890f79a0ae9c6329ad955201df
Saqib <iamsaqib24@gmail.com>
2022-12-29 11:37:16 +0530

    challenge-done

--- fromScrimba/index.js
+++ fromScrimba/index.js
@@ -53,31 +53,65 @@
 
 // challenge,
 
-const navbar = (
-    <nav>
-        <h1>React</h1>
-        <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Docs</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-)
+// const navbar = (
+//     <nav>
+//         <h1>React</h1>
+//         <ul>
+//             <li>Home</li>
+//             <li>Projects</li>
+//             <li>Docs</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
 
-const page = (
-    <div>
-        <h1>I am leaning React from Scrimba</h1>
-        <h3>Reasons I'm learning react</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's a hireable skill</li>
-        </ol>
-    </div>
-)
+// const page = (
+//     <div>
+//         <h1>I am leaning React from Scrimba</h1>
+//         <h3>Reasons I'm learning react</h3>
+//         <ol>
+//             <li>It's composable</li>
+//             <li>It's declarative</li>
+//             <li>It's a hireable skill</li>
+//             <li>It's a most popular js library</li>
+//         </ol>
+//     </div>
+// )
 
 // ReactDOM.render(navbar, document.getElementById("root"))
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(page)
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(page)
+
+
+
+
+/*
+Challenge: Starting from scratch, build and render the
+HTML for our section project. Check the Google slide for
+what you're trying to build.
+
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+  const element = (
+    <div>
+    {/* <img src="./react-logo.png" width= "40px" /> */}
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps including mobile apps</li>
+    </ul>
+    </div>
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(element)

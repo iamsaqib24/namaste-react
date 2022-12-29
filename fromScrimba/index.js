a75afb1ed0783e4d1a4ed3c9dd761c1e46683eea
Saqib <iamsaqib24@gmail.com>
2022-12-29 12:16:56 +0530

    component-challenge-done

--- fromScrimba/index.js
+++ fromScrimba/index.js
@@ -150,19 +150,45 @@ Hints:
 
 // Learning components, Pascal way
 
-function FirstComponent() {
+// function FirstComponent() {
+//     return (
+//         <div>
+//             <h1>Fun facts about React</h1>
+//             <ul>
+//                 <li>Was first released in 2013</li>
+//                 <li>Was originally created by Jordan Walke</li>
+//                 <li>Is maintained by Meta(Facebook)</li>
+//                 <li>Power thousands of enterprise apps including mobile apps</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<FirstComponent />)
+
+
+function Page() {
     return (
         <div>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Is maintained by Meta(Facebook)</li>
-                <li>Power thousands of enterprise apps including mobile apps</li>
-            </ul>
+            <header>
+                <nav>
+                    {/* <img src="./react-logo.png" width="40px" /> */}
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<FirstComponent />)
+root.render(<Page />)

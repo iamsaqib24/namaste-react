7ef8676dabbb50a5e54f4d49664e80b2c906dd54
Saqib <iamsaqib24@gmail.com>
2022-12-29 11:45:43 +0530

    quiz-to-recap

--- fromScrimba/index.js
+++ fromScrimba/index.js
@@ -101,17 +101,48 @@ Hints:
   just like in HTML. In the slide, I have it set to 40px
  */
 
-  const element = (
-    <div>
-    {/* <img src="./react-logo.png" width= "40px" /> */}
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Is maintained by Facebook</li>
-        <li>Power thousands of enterprise apps including mobile apps</li>
-    </ul>
-    </div>
-)
 
-ReactDOM.createRoot(document.getElementById("root")).render(element)
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// const element = (
+//     <div>
+//     {/* <img src="./react-logo.png" width= "40px" /> */}
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//         <li>Was first released in 2013</li>
+//         <li>Was originally created by Jordan Walke</li>
+//         <li>Is maintained by Facebook</li>
+//         <li>Power thousands of enterprise apps including mobile apps</li>
+//     </ul>
+//     </div>
+// )
+
+// ReactDOM.createRoot(document.getElementById("root")).render(element)
+
+// Quiz to recap,
+
+// 1. Why do we need to `import React from "react"` in our files?
+// React is what defines JSX
+
+// 2. If I were to console.log(page) in index.js, what would show up?
+// A JavaScript object. React elements that describe what React should
+// eventually add to the real DOM for us.
+
+// 3. What's wrong with this code:
+// ```
+// const page = (
+//     <h1>Hello</h1>
+//     <p>This is my website!</p>
+// )
+// ```
+// We need our JSX to be nested under a single parent element
+
+// 4. What does it mean for something to be "declarative" instead of "imperative"?
+// Declarative means I can tell the computer WHAT to do
+// and expect it to handle the details. Imperative means I need
+// to tell it HOW to do each step.
+
+// 5. What does it mean for something to be "composable"?
+// We have small pieces that we can put together to make something
+// larger/greater than the individual pieces.

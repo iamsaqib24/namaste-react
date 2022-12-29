f95919fe2c1d88f51bd83f960fcba855226f2830
Saqib <iamsaqib24@gmail.com>
2022-12-29 14:22:31 +0530

    project-navbar-created

--- fromScrimba/index.js
+++ fromScrimba/index.js
@@ -198,52 +198,68 @@ Hints:
 // import React from "react"
 // import Header from "./Header"
 
-function Header() {
-    return (
-        <header>
-            <nav>
-                <img src="./react-icon.png" alt="react-icon" className="nav-logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+// function Header() {
+//     return (
+//         <header>
+//             <nav>
+//                 <img src="./react-icon.png" alt="react-icon" className="nav-logo" />
+//                 <ul className="nav-items">
+//                     <li>Pricing</li>
+//                     <li>About</li>
+//                     <li>Contact</li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
 
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-        </div>
-    )
-}
+// function MainContent() {
+//     return (
+//         <div>
+//             <h1>Reasons I'm excited to learn React</h1>
+//             <ol>
+//                 <li>It's a popular library, so I'll be
+//                 able to fit in with the cool kids!</li>
+//                 <li>I'm more likely to get a job as a developer
+//                 if I know React</li>
+//             </ol>
+//         </div>
+//     )
+// }
 
-function Footer() {
-    return (
-        <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
-    )
-}
+// function Footer() {
+//     return (
+//         <footer>
+//                 <small>© 2021 Ziroll development. All rights reserved.</small>
+//             </footer>
+//     )
+// }
 
-function Page() {
+// function Page() {
+//     return (
+//         <div>
+//             <Header />
+//             <MainContent />
+//             <Footer />
+//         </div>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<Page />)
+
+
+// Project - 1
+
+function Navbar() {
     return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
+        <nav>
+            <img src="./react-icon.png" alt="react-icon" className="nav-logo" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Page />)
+root.render(<Navbar />)

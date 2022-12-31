8e1037971e480006454fb7d32d008a0fee72a457
Saqib <iamsaqib24@gmail.com>
2022-12-31 22:16:28 +0530

    component

--- week-02/lec-02/App.js
+++ week-02/lec-02/App.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom/client"
 
 const heading = React.createElement("h1", { className: "heading"}, "Igniting our App")
 
+
+function Header() {
+    return (
+        <div>
+            <h1 className="component-heading">Component Header</h1>
+        </div>
+    )
+}
+
+
+
+function Page() {
+    return (
+        <Header />
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(<Page />)

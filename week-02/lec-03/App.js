b45c7a6e1895455c10b9616671399c67ad7c07a8
Saqib <iamsaqib24@gmail.com>
2023-01-02 17:47:12 +0530

    learning-jsx

--- week-02/lec-03/App.js
+++ week-02/lec-03/App.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client"
 
 const heading = React.createElement("h1", {className: "heading"}, "Practice")
 
+
+
+// JSX - this is HTML like syntax but it is not HTML inside javascript. It is a syntax extension to JavaScript.
+
+const heading2 = (
+    <>
+    <h1 className='heading'>
+        Hello JSX
+    </h1>
+    <p>This is HTML like syntax but it is not HTML inside javascript. It is a syntax extension to JavaScript.</p>
+    </>
+)
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(heading2)
